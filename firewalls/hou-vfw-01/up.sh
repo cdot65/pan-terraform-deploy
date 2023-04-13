a72bb1067ce2e039c3095c6d1d7a216daf0694d7
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-13 08:53:39 -0500

    init

--- firewalls/hou-vfw-01/up.sh
+++ firewalls/hou-vfw-01/up.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 while true; do
-    if [[ $(curl -s -o /dev/null -w '%{http_code}' -k https://13.85.187.209/php/login.php?) == "200" ]]; then
+    if [[ $(curl -s -o /dev/null -w '%{http_code}' -k https://20.118.112.200/php/login.php?) == "200" ]]; then
         echo "200 OK received"
         break
     else
