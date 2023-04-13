0d0f7732fa18a5ed924b6025cb642e76757c5d9f
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-13 09:28:05 -0500

    pass IP as argument

--- firewalls/hou-vfw-01/up.sh
+++ firewalls/hou-vfw-01/up.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
+
+IP_ADDRESS=$1
+
+if [ -z "$IP_ADDRESS" ]; then
+    echo "Usage: $0 <IP_ADDRESS>"
+    exit 1
+fi
+
 while true; do
-    if [[ $(curl -s -o /dev/null -w '%{http_code}' -k https://20.118.112.200/php/login.php?) == "200" ]]; then
+    if [[ $(curl -s -o /dev/null -w '%{http_code}' -k https://"$IP_ADDRESS"/php/login.php?) == "200" ]]; then
         echo "200 OK received"
         break
     else
